603ea0e10cf957d59aea5263fe925e400256ccfc
thiagofddias <thiagofdias2@gmail.com>
2024-11-25 17:49:50 -0300

    Finalizado

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,26 +1,27 @@
-import styles from '../styles/Porta.module.css';
-import PortaModel from '../model/porta';
-import React from 'react';
+import styles from "../styles/Porta.module.css"
+import Presente from './Presente'
+import PortaModel from '../model/porta'
+import React from "react"
 
 interface PortaProps {
-    value: PortaModel;
-    onChange: (novaPorta: PortaModel) => void;
+    value: PortaModel
+    onChange: (novaPorta: PortaModel) => void
 }
 
 export default function Porta(props: PortaProps) {
-    const porta = props.value;
-    const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : '';
+    const porta = props.value
+    const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : ''
 
-    const alterarSelecao = e => props.onChange(porta.alternarSelecao());
+    const alternarSelecao = e => props.onChange(porta.alternarSelecao())
     const abrir = e => {
-        e.stopPropagation();
+        e.stopPropagation()
         props.onChange(porta.abrir())
-    };
+    }
 
     function renderizarPorta() {
         return (
             <div className={styles.porta}>
-                <div className={styles.numero}>{[porta.numero]}</div>
+                <div className={styles.numero}>{porta.numero}</div>
                 <div className={styles.macaneta}
                     onClick={abrir}></div>
             </div>
@@ -28,9 +29,12 @@ export default function Porta(props: PortaProps) {
     }
 
     return (
-        <div className={styles.area} onClick={alterarSelecao}>
+        <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.estrutura} ${selecionada}`}>
-                {porta.aberta ? false : renderizarPorta()}
+                {porta.fechada ?
+                    renderizarPorta() : 
+                    porta.temPresente ? <Presente></Presente> : null
+                }
             </div>
             <div className={styles.chao}></div>
         </div>
